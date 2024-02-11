429d1519a55d98c845bdfdec8fdc640912b188a1
CSSkelton <codysskelton@gmail.com>
2024-02-11 12:32:47 -0500

    week 5 python exercise

---
+++ week-5/skelton_hobbies.py
@@ -0,0 +1,19 @@
+# list of hobbies
+hobbies = ["haunt", "leathercraft", "video games", "d&d", "cooking"]
+
+# loop through hobbies list
+for x in hobbies:
+    print(x)
+
+# list of days
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+# loop through days and get weekends off
+i = 0
+
+for x in days:
+    if i == 5 or i == 6:
+        print(x + ": You are off. Enjoy your hobbies")
+    else:
+        print(x + ": It is a work day")
+    i += 1
