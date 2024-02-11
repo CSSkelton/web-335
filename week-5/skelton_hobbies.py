89df1742e891fbcaaf7aedcb94504eaa947437f4
CSSkelton <codysskelton@gmail.com>
2024-02-11 12:56:43 -0500

    code attribution

--- week-5/skelton_hobbies.py
+++ week-5/skelton_hobbies.py
@@ -1,3 +1,11 @@
+#
+# Title: skelton_hobbies.py
+# Author: Cody Skelton
+# Date: 02.11.2024
+# Purpose: Loops practice 
+# Requirements from WEB 335 Exercise 5.3
+#
+
 # list of hobbies
 hobbies = ["haunt", "leathercraft", "video games", "d&d", "cooking"]
 
